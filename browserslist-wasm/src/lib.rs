72867ab31b49eb8f19e3513102e57d820eaade71
Alexey Vasiliev <leopard.not.a@gmail.com>
2021-12-21 20:12:23 +0200

    add interface

--- browserslist-wasm/src/lib.rs
+++ browserslist-wasm/src/lib.rs
@@ -1,7 +1,7 @@
 mod utils;
 
-use wasm_bindgen::prelude::*;
 use browserslist::{Opts, resolve};
+use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,49 @@
 <script>
-  import init, {browserslist_wasm as browserslistWasm} from './../browserslist-wasm/pkg'
+  import {memoize} from './memoize'
+  import init, {browserslist_wasm as browserslistWasm} from './wasm'
 
-  init().then(() => {
-    console.log(browserslistWasm('>1%, Firefox ESR, not dead, not ie 11, not op_mini all'))
-  })
+  let browserInput = '>0.3%, Firefox ESR, not dead'
+
+  const loadWasm = memoize(init)
+
+  const getBrowserList = (str) => {
+    try {
+      return JSON.stringify(browserslistWasm(str))
+    } catch (e) {
+      return e.message
+    }
+  }
 </script>
 
 <style>
+  .input-wrapper {
+    width: 100%;
+  }
 
+  .input {
+    box-sizing: border-box;
+    margin: 0.5rem;
+    font-size: 2rem;
+    width: 90%;
+  }
+
+  .result {
+    margin: 0.5rem 0;
+    font-size: 2rem;
+  }
 </style>
 
 <main>
-
+  <div class="input-wrapper">
+    <input class="input" bind:value={browserInput} />
+  </div>
+  <div class="result">
+    {#await loadWasm()}
+      <p>Load wasm...</p>
+    {:then}
+      Result: {getBrowserList(browserInput)}
+    {:catch error}
+      <p style="color: red">{error.message}</p>
+    {/await}
+  </div>
 </main>
