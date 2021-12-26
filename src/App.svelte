6856c1d19762d59b08a533bcc414d48007acedf5
Alexey Vasiliev <leopard.not.a@gmail.com>
2021-12-26 20:37:35 +0200

    fixed browserlist version

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,33 @@
 <script>
-  import {memoize} from './memoize'
+  import { onMount } from 'svelte'
+  import {memoize} from './utils/memoize'
   import init, {browserslist_wasm as browserslistWasm} from './wasm'
   import ReloadPrompt from './ReloadPrompt.svelte'
 
   let browserInput = '>0.3%, Firefox ESR, not dead'
+  let browsersResult = ''
+  let timeout = null
 
   const loadWasm = memoize(init)
 
-  const getBrowserList = (str) => {
+  const getBrowserList = () => {
     try {
-      return JSON.stringify(browserslistWasm(str))
+      return JSON.stringify(browserslistWasm(browserInput))
     } catch (e) {
       return e.message
     }
   }
+
+  const debounceBrowserList = (wait = 300) => {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      browsersResult = getBrowserList()
+    }, wait)
+  }
+
+  onMount(() => {
+    debounceBrowserList()
+  })
 </script>
 
 <style>
@@ -35,17 +49,18 @@
 </style>
 
 <main>
-  <ReloadPrompt />
   <div class="input-wrapper">
-    <input class="input" bind:value={browserInput} />
+    <input class="input" bind:value={browserInput} on:keyup={() => debounceBrowserList()} />
   </div>
   <div class="result">
     {#await loadWasm()}
       <p>Load wasm...</p>
     {:then}
-      Result: {getBrowserList(browserInput)}
+      Result: {browsersResult}
     {:catch error}
       <p style="color: red">{error.message}</p>
     {/await}
   </div>
 </main>
+
+<ReloadPrompt />
